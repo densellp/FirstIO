c68cad23fd408b9aa05d6290796eff94539ebed0
densellp <densell321@gmail.com>
2022-09-12 11:44:42 -0700

    Added main.cpp to main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    //declare variables
+    
+    cout << "HEy" << endl;
+    //algorithm
+    //this is not valid c++ code!
+    
+    return 0;
+}
